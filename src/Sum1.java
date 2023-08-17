7e993b2b37b980c8449d3458d88e85af0ad0fe28
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 09:43:21 +0530

    First commit....

---
+++ src/Sum1.java
@@ -0,0 +1,14 @@
+
+public class Sum1 {
+	public static void main(String[] args)
+	{
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat");
+		else
+		{
+			System.out.println("Do work");
+		}
+	}
+
+}
